ce5367ea3f5eb6b980a9f4b719334dcac56d15ba
IrynaKniazieva <gleb0812@gmail.com>
2022-08-17 00:30:33 +0300

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,40 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
+
+
+const elements = ingredients.map(ingredientsItemEl => {
+  const ingredientItemEl = document.createElement('li');
+  ingredientItemEl.classList.add('item');
+  ingredientItemEl.textContent = ingredientsItemEl;
+
+  return ingredientItemEl;
+});
+const ingredientsEl = document.querySelector('#ingredients');
+ingredientsEl.append(...elements);
+
+
+// const elements = [];
+
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const ingredient = ingredients[i];
+
+//   const ingredientsItemEl = document.createElement('li');
+// ingredientsItemEl.classList.add('item');
+// ingredientsItemEl.textContent = ingredient;
+
+// elements.push(ingredientsItemEl);
+// };
+
+
+// const ingredientsEl = document.querySelector('#ingredients');
+// ingredientsEl.append(...elements);
+
+
+
+

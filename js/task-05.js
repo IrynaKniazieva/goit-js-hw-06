3c5d6773adacbd3c308468a34ea2b53f71a4ae79
IrynaKniazieva <gleb0812@gmail.com>
2022-08-18 23:27:37 +0300

    Task - 5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const inputEl = document.querySelector('input');
+const nameLabel = document.querySelector('h1>span');
+
+console.log(inputEl);
+console.log(nameLabel);
+
+inputEl.addEventListener('input', onInputChange);
+
+function onInputChange (event) {
+    nameLabel.textContent = event.currentTarget.value;
+};

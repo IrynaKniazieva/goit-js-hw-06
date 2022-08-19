22232b7015b6a32a4173435874bbd896e7598934
IrynaKniazieva <gleb0812@gmail.com>
2022-08-20 02:02:43 +0300

    Task - 9 start

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const buttonChangColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+const backgroundColor = document.querySelector('.widget');
+
+buttonChangColor.addEventListener('click', onColorChange);
+
+function onColorChange (event) {
+  backgroundColor.style = getRandomHexColor;
 }
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;;
+}
+
+

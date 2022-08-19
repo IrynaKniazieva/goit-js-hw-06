bb83586929f20f5e309aedbb1dc36ac500f0c793
IrynaKniazieva <gleb0812@gmail.com>
2022-08-20 01:06:53 +0300

    Task - 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector('.login-form');
 
+console.log(formEl);
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault()
+    const {
+        elements: { email, password }
+      } = event.currentTarget;
+
+      if (email.value === "" || password.value === "") {
+        return alert("Пожалуйста, заполните все поля!");
+      }
+
+      console.log(`Email: ${email.value}, Password: ${password.value}`);
+      event.currentTarget.reset();
+}

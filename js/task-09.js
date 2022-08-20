f11353d784a032185a6bf58bdf729559278e5d7b
IrynaKniazieva <gleb0812@gmail.com>
2022-08-20 15:24:20 +0300

    Task - 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,18 +1,32 @@
 const buttonChangColor = document.querySelector('.change-color');
 const spanColor = document.querySelector('.color');
 
+console.log(buttonChangColor);
+console.log(spanColor);
 
-buttonChangColor.addEventListener('click', onColorChange);
+buttonChangColor.addEventListener('click', () => {
+  const hexColor = getRandomHexColor();
+  document.body.style.backgroundColor = hexColor;
+  spanColor.textContent = hexColor;
 
-function onColorChange () {
-  document.body.style.backgroundColor = (getRandomHexColor);
-  spanColor.textContent = getRandomHexColor;
-}
+})
 
+// buttonChangColor.addEventListener('click', onColorChange);
+
+// function onColorChange () {
+//   document.body.style.backgroundColor = getRandomHexColor();
+//   spanColor.textContent = getRandomHexColor();
+// }
+
+// randomColor из VSC
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 
+// randomColor из ТЗ
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;;
+// }
